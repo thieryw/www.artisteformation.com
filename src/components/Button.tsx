b707a39abc62fdf38b6754e16fab78c9fe387ad2
thieryw <39378411+thieryw@users.noreply.github.com>
2023-11-28 15:27:57 +0000

    start building button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import { memo } from "react";
+import { Link } from "../tools/link";
+
+
+export type ButtonProps = Link;
+
+
+
+
+export const Button = memo((props: ButtonProps) => {
+    //const {href, label, onClick} = props;
+    return <button>
+
+    </button>
+})
