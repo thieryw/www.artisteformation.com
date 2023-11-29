124fc51ebc9964d007bc921461d4a0ad4df04e0e
thieryw <39378411+thieryw@users.noreply.github.com>
2023-11-29 23:30:24 +0000

    remove default margin from body

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import { RouteProvider } from "./router";
+import "./theme/global.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouteProvider>
-      <App />
+        <App />
     </RouteProvider>
   </React.StrictMode>,
 )
