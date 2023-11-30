3902686e80cc67ea3e6a86d44ed62709ed1d7ebc
thieryw <39378411+thieryw@users.noreply.github.com>
2023-11-30 23:57:17 +0000

    LinkButton filled variant

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -6,6 +6,7 @@ import { tss, Text } from "../theme";
 export type LinkButtonProps = Link & {
     className?: string;
     classes?: Partial<ReturnType<typeof useStyles>["classes"]>
+    variant?: "filled" | "outlined"
 }
 
 
@@ -13,20 +14,33 @@ export type LinkButtonProps = Link & {
 
 
 export const LinkButton = memo((props: LinkButtonProps) => {
-    const { href, label, onClick, className } = props;
+    const { href, label, onClick, className, variant } = props;
     const { classes, cx } = useStyles({
+        "variant": variant ?? "outlined",
         "classesOverrides": props.classes
     });
     return <a className={cx(classes.root, className)} onClick={onClick} href={href}>
+        <div className={classes.inner}>
+
             <Text className={classes.linkLabel} typo="sectionPageOrButton">{label}</Text>
-        </a>
+        </div>
+    </a>
 });
 
-const useStyles = tss.withNestedSelectors<"linkLabel">().create(({ theme, classes }) => ({
+const useStyles = tss.withParams<Required<Pick<LinkButtonProps, "variant">>>().withNestedSelectors<"linkLabel">().create(({ theme, classes, variant }) => ({
     "root": {
-        "padding": "25px 50px 25px 50px",
         "textDecoration": "none",
+    },
+    "inner": {
+        "padding": "25px 50px 25px 50px",
         "border": `solid ${theme.colors.bloodOrange} 1px`,
+        "backgroundColor": (()=>{
+            switch(variant){
+                case "filled" : return theme.colors.bloodOrange;
+                case "outlined": return undefined;
+            }
+
+        })(),
         "transition": "background-color 300ms",
         [`&:hover .${classes.linkLabel}`]: {
             "color": theme.colors.white
@@ -34,9 +48,16 @@ const useStyles = tss.withNestedSelectors<"linkLabel">().create(({ theme, classe
         ":hover": {
             "backgroundColor": theme.colors.darkGray
         }
+
     },
     "linkLabel": {
-        "color": theme.colors.indigo,
+        "color": (()=>{
+            switch(variant){
+                case "filled": return theme.colors.white;
+                case "outlined": return theme.colors.indigo;
+            }
+
+        })(),
         "textTransform": "uppercase",
         "marginBlock": 0,
         "transition": "color 300ms"
