2bcba5e41f61a790422fb49d690fbf4fa12d4398
thieryw <39378411+thieryw@users.noreply.github.com>
2023-12-01 15:48:06 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      "targets": ['defaults', 'not IE 11'],
+    })
+  ],
   "base": "/www.artisteformation.com/"
 })
 
